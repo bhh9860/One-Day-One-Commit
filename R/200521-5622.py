76749432aae29b04f73e7af553fdaab235c288c8
bhh9860 <34750753+bhh9860@users.noreply.github.com>
2020-05-21 20:55:36 +0900

    놀라운 깨달음을 얻어버렸다

---
+++ R/200521-5622.py
@@ -0,0 +1,11 @@
+alpha = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
+beta = [2, 2, 2, 3, 3, 3, 4, 4, 4, 5, 5, 5, 6, 6, 6, 7, 7, 7, 7, 8, 8, 8, 9, 9, 9, 9]
+number = []
+
+a = list(input())
+for i in a:
+	number.append(beta[alpha.index(i)])
+	
+print(sum(number)+len(number))
+
+
