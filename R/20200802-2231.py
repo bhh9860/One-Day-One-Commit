6dde9802f39a19111bbf7c40933233363556b06b
bhh9860 <34750753+bhh9860@users.noreply.github.com>
2020-08-02 23:26:42 +0900

    걱정했지만 의외로 쉽게 풀림. bp와 시간복잡도를 한번 공부해볼 필요성을 느낌.

---
+++ R/20200802-2231.py
@@ -0,0 +1,20 @@
+def bun(num): #분해함수
+	a = list(str(num)) 
+	a.append(num)
+	a = list(map(int, a)) 
+	return sum(a)
+'''a = sum(map(int, str(num)))
+	 return a + num 로 대체가능'''
+	
+a = int(input())
+if a < 10:
+	print('0') #한자릿수는 자기자신이 생성자가 될 수 없다.
+else:
+	b = 0
+	for i in range(1, a+1):
+		if a == bun(i):
+			print(i)
+			b += 1
+			break
+	if b != 1: #b가 1이 아니면 생성자가 없음.
+		print('0')
