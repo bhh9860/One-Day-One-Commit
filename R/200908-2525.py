3a9c8b5b078c1c702b37820b07c5aa972138cb4b
Jeong Hyeon Tang <34750753+bhh9860@users.noreply.github.com>
2020-09-09 00:16:06 +0900

    수고했어 오늘도

--- R/200908-2525.py
+++ R/200908-2525.py
@@ -1,1 +1,10 @@
-#solving
+si, bun = list(map(int, input().split()))
+plus = int(input())
+bun += plus
+if bun > 59:
+	a = bun // 60
+	bun %= 60
+	si += a
+if si > 23:
+	si %= 24
+print('{0} {1}'.format(si, bun))
