720aec9c06a513ff77a4da3c3eebe2b0a672edfe
Jeong Hyeon Tang <34750753+bhh9860@users.noreply.github.com>
2020-11-26 20:01:35 +0900

    유클리드 호제법 하나를 아니 여러가지 문제를 풀 수 있구만 껄껄

--- R/201126-1934.py
+++ R/201126-1934.py
@@ -1,11 +1,14 @@
-for i in range(int(input())):
-	a, b = list(map(int, input().split()))
-	if a < b:
-		a, b = b, a
-	c, d = a, b
-	while b != 0:
-		n = a % b
-		a = b
-		b = n
-	
-	print(c*d//a)
+for _ in range(int(input())):
+	ans = 0
+	a = list(map(int, input().split()))
+	a[1:] = reversed(a[1:])
+	for i in range(a.pop(0)-1):
+		for j in range(i+1, len(a)):
+			x = a[i]
+			y = a[j]
+			while y != 0:
+				n = x % y
+				x = y
+				y = n
+			ans += x
+	print(ans)
